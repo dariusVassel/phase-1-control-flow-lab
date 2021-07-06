2f1ba39091e268c90332b06409926c5b6a91caa4
+ <+>
2021-07-06 16:22:45 -0400

    Complete

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue) {
   // Write your code here!
+  if (someValue <= 400) {
+    return ("This one is on me!")
+  } else if (someValue > 2000 && someValue < 2500) {
+    return ("I will gladly take your thirty bucks.")
+  } else if (someValue > 2500) {
+    return ("No can do.")
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(someCity) {
   // Write your code here!
+  return someCity === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(someTip) {
   // Write your code here!
+  let charmLevel
+  switch (someTip) {
+    case 'generous': return charmLevel = "Thank you so much.";
+    case 'not as generous': return charmLevel = "Thank you.";
+    default:
+      return charmLevel = "Bye.";
+
+  }
+
 }
+
